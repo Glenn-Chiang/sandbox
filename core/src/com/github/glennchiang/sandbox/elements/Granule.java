67b432306b8601e7e0243c7c32514fea36fe6912
Glenn Chiang <glennchiang24@gmail.com>
2024-06-12 20:44:32 +0800

    Create Granule abstract class which Sand inherits from instead of inheriting from solid Move Sand update behaviour into Granule class

---
+++ core/src/com/github/glennchiang/sandbox/elements/Granule.java
@@ -0,0 +1,51 @@
+package com.github.glennchiang.sandbox.elements;
+
+import com.github.glennchiang.sandbox.Direction;
+import com.github.glennchiang.sandbox.Grid;
+import com.github.glennchiang.sandbox.utils.RandomUtils;
+
+public abstract class Granule extends Element {
+    protected final int fallRate; // Number of cells by which the element will move down per frame
+
+    public Granule(Grid grid) {
+        super(grid);
+        fallRate = getFallRate();
+    }
+    protected abstract int getFallRate();
+
+    @Override
+    protected boolean sinksIn(Element element) {
+        return element instanceof Liquid;
+    }
+
+    @Override
+    public void update(int row, int col) {
+        Direction targetDirection = Direction.DOWN;
+
+        if (isCellEmpty(targetDirection)) {
+            move(targetDirection, fallRate);
+            return;
+        }
+
+        // If below is water, sink in water by swapping places with it
+        if (getElementAt(targetDirection) instanceof Water) {
+            swap(targetDirection);
+            return;
+        }
+
+        boolean canMoveDownLeft = isCellEmpty(Direction.DOWN_LEFT);
+        boolean canMoveDownRight = isCellEmpty(Direction.DOWN_RIGHT);
+
+        if (canMoveDownLeft && canMoveDownRight) {
+            targetDirection = RandomUtils.selectRandom(Direction.DOWN_LEFT, Direction.DOWN_RIGHT);
+        }
+        else if (canMoveDownLeft) {
+            targetDirection = Direction.DOWN_LEFT;
+        }
+        else if (canMoveDownRight) {
+            targetDirection = Direction.DOWN_RIGHT;
+        }
+
+        move(targetDirection, fallRate);
+    }
+}
--- core/src/com/github/glennchiang/sandbox/elements/Sand.java
+++ core/src/com/github/glennchiang/sandbox/elements/Sand.java
@@ -4,40 +4,14 @@ import com.github.glennchiang.sandbox.Direction;
 import com.github.glennchiang.sandbox.Grid;
 import com.github.glennchiang.sandbox.utils.RandomUtils;
 
-public class Sand extends Solid {
+public class Sand extends Granule {
+    private final int fallRate = 1;
     public Sand(Grid grid) {
         super(grid);
     }
-    private final int fallSpeed = 1; // Max number of cells the element will fall per frame
 
     @Override
-    public void update(int row, int col) {
-        Direction targetDirection = Direction.DOWN;
-
-        if (isCellEmpty(targetDirection)) {
-            move(targetDirection, fallSpeed);
-            return;
-        }
-
-        // If below is water, sink in water by swapping places with it
-        if (getElementAt(targetDirection) instanceof Water) {
-            swap(targetDirection);
-            return;
-        }
-
-        boolean canMoveDownLeft = isCellEmpty(Direction.DOWN_LEFT);
-        boolean canMoveDownRight = isCellEmpty(Direction.DOWN_RIGHT);
-
-        if (canMoveDownLeft && canMoveDownRight) {
-            targetDirection = RandomUtils.selectRandom(Direction.DOWN_LEFT, Direction.DOWN_RIGHT);
-        }
-        else if (canMoveDownLeft) {
-            targetDirection = Direction.DOWN_LEFT;
-        }
-        else if (canMoveDownRight) {
-            targetDirection = Direction.DOWN_RIGHT;
-        }
-
-        move(targetDirection, fallSpeed);
+    protected int getFallRate() {
+        return fallRate;
     }
 }
